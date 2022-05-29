b3fb6e9a6b454ce75a6fc77bce02009dd28ecdce
QyuriLa <rea9lizer@gmail.com>
2022-05-30 00:47:46 +0900

    Update qyurila-cornesuki keymap

--- keyboards/crkbd/keymaps/qyurila-cornesuki/layers.h
+++ keyboards/crkbd/keymaps/qyurila-cornesuki/layers.h
@@ -1,15 +1,14 @@
 #pragma once
 
-#define _BASE 0
-#define _UTIL 1
-#define _GAME 2
-#define _ARROW 3
-
-#define _NUMBER 4
-#define _NUMPAD 5
-
-#define _EXTEND 6
-#define _MOUSE 7
-
-#define _FUNC 8
-#define _LOCK 9
+enum layers {
+    _BASE,
+    _UTIL,
+    _GAME,
+    _ARROW,
+    _NUMBER,
+    _NUMPAD,
+    _EXTEND,
+    _MOUSE,
+    _FUNC,
+    _LOCK,
+};
